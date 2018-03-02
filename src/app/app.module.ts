5db9f0c8c14c9d1777372fc347881ec4af2a97da
pquantum <pr.pascal.bachmann@gmail.com>
2018-03-02 08:59:56 +0100

    add material form input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }          from '@angular/platform-browser';
+import { NgModule }               from '@angular/core';
+import {BrowserAnimationsModule}  from '@angular/platform-browser/animations';
 
 
-import { AppComponent } from './app.component';
-import { FormIoComponent } from './form-io/form-io.component';
-import { HeadbarComponent } from './headbar/headbar.component';
-import { FormIoListComponent } from './form-io-list/form-io-list.component';
+import { AppComponent }           from './app.component';
+import { FormIoComponent }        from './form-io/form-io.component';
+import { HeadbarComponent }       from './headbar/headbar.component';
+import { FormIoListComponent }    from './form-io-list/form-io-list.component';
 import { KikoFormFieldComponent } from './kiko-form-field/kiko-form-field.component';
+
+import {MatInputModule}           from '@angular/material';
 
 
 @NgModule({
@@ -18,7 +21,12 @@ import { KikoFormFieldComponent } from './kiko-form-field/kiko-form-field.compon
     KikoFormFieldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
