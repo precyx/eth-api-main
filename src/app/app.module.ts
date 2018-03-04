9e9c8a6e3f54444dd7620b7a442b41cdb2026c66
pquantum <pr.pascal.bachmann@gmail.com>
2018-03-04 19:41:02 +0100

    fix import bug, add trackbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { BrowserModule }              from '@angular/platform-browser';
+
 import { NgModule }                   from '@angular/core';
 import { BrowserAnimationsModule }    from '@angular/platform-browser/animations';
-import { FormsModule }                from '@angular/forms';
+import { FormsModule }        from '@angular/forms';
 import { MatInputModule }             from '@angular/material';
 
 import { AppComponent }               from './app.component';
-import { AbiDetailComponent }         from './abi-detail/form-io.component';
+
+import { AppRoutingModule }           from './app-routing.module';
+
 import { HeadbarComponent }           from './headbar/headbar.component';
 import { KikoFormFieldComponent }     from './kiko-form-field/kiko-form-field.component';
 import { AbiOverviewComponent }       from './abi-overview/abi-overview.component';
+import { AbiDetailComponent }         from './abi-detail/abi-detail.component';
 import { NewContractComponent }       from './new-contract/new-contract.component';
 import { ContractOverviewComponent }  from './contract-overview/contract-overview.component';
 
 import { DataService }                from './data.service';
 import { Web3Service }                from './web3.service';
-import { CommunicationService }       from './communication.service';
 
 import { SafeUrlPipe }                from './shared/security/safe-url.pipe';
 import { CallbackPipe }               from './shared/pipes/callback.pipe';
-
-import { AppRoutingModule }           from './app-routing.module';
-
 
 
 
@@ -40,15 +40,14 @@ import { AppRoutingModule }           from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatInputModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule
   ],
   exports: [
     MatInputModule
   ],
   providers: [
     DataService,
-    CommunicationService,
     Web3Service
   ],
   bootstrap: [AppComponent]
